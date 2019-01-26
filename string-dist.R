fb4a1cee122488fa0dce6edff94fd1df62f1f0dd
Brian Stamper <brianstamper10@gmail.com>
2019-01-25 23:20:15 -0500

    Create a pop sample

--- string-dist.R
+++ string-dist.R
@@ -1,4 +1,18 @@
+library(tidyverse)
+library(babynames)
+data(babynames)
 
-Rcpp::sourceCpp('cross_join.cpp')
-Rcpp::sourceCpp('mod_hamming_dist.cpp')
+Rcpp::sourceCpp('grams.cpp')
 
+fnames <- babynames %>%
+  filter(year %in% 1940:2000) %>%
+  group_by(name) %>%
+  summarise(wt = sum(prop)) %>%
+  sample_n(10, weight = wt) %>%
+  mutate_all(toupper) %>%
+  select(name_a = name) %>%
+  mutate(name_b = name_a)
+
+name_pairs <- cross_df(fnames)
+
+k <- grams(fnames$name_a)
